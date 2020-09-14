c549278f2ae9e926d920f84a251da56466477643
kristianjackson <kristian.jackson@gmail.com>
2020-09-14 16:58:05 +0000

    migrated to new database, including new environment vars

--- web_project/settings.py
+++ web_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_DB'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
